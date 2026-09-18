bb4ee6f878d35130e6ade8f123545d59ffa4a866
meklit Tadesse <meklitt.16@gmail.com>
2026-09-18 16:35:50 -0300

    complete exercise 4

---
+++ exercise/app/menu/DishList.js
@@ -0,0 +1,12 @@
+export default function DishList(){
+    return(
+        <div>
+            <h1>Dishes</h1>
+            <ul>
+                <li>Doro wer</li>
+                <li>kitfo</li>
+                <li>Shiro</li>
+            </ul>
+        </div>
+    );
+}
---
+++ exercise/app/menu/catagoryBar.js
@@ -0,0 +1,12 @@
+export default function CategoryBar(){
+    return(
+        <div>
+            <h2>categories</h2>
+
+            <button>All</button>
+            <button>Chicken</button>
+            <button>Beef</button>
+            <button>Dessert</button>
+        </div>
+    )
+}
