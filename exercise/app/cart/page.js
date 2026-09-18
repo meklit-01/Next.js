6c65d23eb363a219ce7b8d8870dc24c7e3d34b54
meklit Tadesse <meklitt.16@gmail.com>
2026-09-18 16:15:51 -0300

    complete exercise 2

---
+++ exercise/app/cart/page.js
@@ -0,0 +1,8 @@
+export default function Cart(){
+    return(
+        <div>
+            <h1>Cart</h1>
+            <p>Your Cart is Here</p>
+        </div>
+    )
+}
---
+++ exercise/app/checkout/page.js
@@ -0,0 +1,8 @@
+export default function Checkout(){
+    return(
+        <div>
+            <h1>Checkout</h1>
+            <p>Complete your order here.</p>
+        </div>
+    )
+}
